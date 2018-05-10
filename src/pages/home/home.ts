81337e4d295f8c77f13a78f753fadbaa71ac21a9
bittersweet95 <woraluk95@gmail.com>
2018-05-10 10:05:14 +0700

    รับข้อมูลมาจาก api ใน codeinigter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  vessels=[];
+  vesselName;
+  constructor(public navCtrl: NavController, public http:HttpClient) {
+    
   }
-
+public loadData(vesselName){
+ // this.vessels.push(vesselName);
+  //this.vesselName = "";
+  let url="http://localhost/app/index.php/api/fishingvessel/all_ship";
+  this.http.get(url)
+           .subscribe(
+             (result:any) => {
+               console.log(result);
+               this.vessels = result;
+             }
+           );
+}
 }
